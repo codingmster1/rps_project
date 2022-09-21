07d9e7d1b58a2c0780085d94c30efeb8cd7bc3b1
Jeff <doylejeff98@gmail.com>
2022-09-21 14:42:47 -0400

    commit

--- rps.js
+++ rps.js
@@ -3,3 +3,6 @@ const computerScore = 0;
 const userScore_span = document.GetElementById("user-score");
 const computerScore_span = document.GetElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
+const rock_div = document.GetElementById("r");
+const paper_div = document.GetElementById("p");
+const scissors_div = document.GetElementById("s");
