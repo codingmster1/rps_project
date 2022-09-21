0d75406b8fa37fc48170d31327c1dfb75d2e702c
Jeff <doylejeff98@gmail.com>
2022-09-21 14:54:06 -0400

    commit

--- rps.js
+++ rps.js
@@ -1,8 +1,15 @@
 const userScore = 0;
 const computerScore = 0;
-const userScore_span = document.GetElementById("user-score");
-const computerScore_span = document.GetElementById("computer-score");
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const rock_div = document.GetElementById("r");
-const paper_div = document.GetElementById("p");
-const scissors_div = document.GetElementById("s");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+
+
+rock_div.addEventListener('click', function()
+{
+  console.log("you clicked on rock");
+})
