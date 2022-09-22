446a4e00653899789d067207b6bf2c9e9a6aaff0
Jeff <doylejeff98@gmail.com>
2022-09-22 13:35:53 -0400

    commit

--- rps.js
+++ rps.js
@@ -7,6 +7,9 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function game(userChoice){
+  
+}
 
 function main()
 {
@@ -25,3 +28,5 @@ scissors_div.addEventListener('click', function()
   game("s");
 })
 }
+
+main();
